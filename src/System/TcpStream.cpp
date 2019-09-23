a6d32ef3259a561887f859e0407d68e7da89debd
darkharvest <kahthan@gmail.com>
2019-09-23 13:08:29 +0530

    Update TcpStream.cpp

--- src/System/TcpStream.cpp
+++ src/System/TcpStream.cpp
@@ -4,6 +4,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <iostream>
 #include "TcpStream.h"
 #include <System/TcpConnection.h>
 
